746120b25a4a87b4c09abf654cee60c9edbc4ba2
Eva <evajavaloych@gmail.com>
2016-09-21 12:11:44 +0200

    Cambio la configuracion para static

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] 
